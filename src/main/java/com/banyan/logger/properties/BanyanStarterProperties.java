25691fcdde3f215f1c41a78869fe42fb94ae9039
ling976 <2731787554@qq.com>
2020-08-16 21:58:17 +0800

    添加默认文件名

--- src/main/java/com/banyan/logger/properties/BanyanStarterProperties.java
+++ src/main/java/com/banyan/logger/properties/BanyanStarterProperties.java
@@ -18,7 +18,7 @@ public class BanyanStarterProperties {
 	/**
 	 * 文件名
 	 */
-	private String fileNamePattern;
+	private String fileNamePattern = "info.%d{yyyy-MM-dd}.%i.log";
 	/**
 	 * 单文件大小
 	 */
@@ -27,30 +27,37 @@ public class BanyanStarterProperties {
 	 * 文件保存时间(天)
 	 */
 	private int maxHistory = 10;
-	
+
 	public String getPath() {
 		return path;
 	}
+
 	public void setPath(String path) {
 		this.path = path;
 	}
+
 	public String getFileNamePattern() {
 		return fileNamePattern;
 	}
+
 	public void setFileNamePattern(String fileNamePattern) {
 		this.fileNamePattern = fileNamePattern;
 	}
+
 	public String getMaxFileSize() {
 		return maxFileSize;
 	}
+
 	public void setMaxFileSize(String maxFileSize) {
 		this.maxFileSize = maxFileSize;
 	}
+
 	public int getMaxHistory() {
 		return maxHistory;
 	}
+
 	public void setMaxHistory(int maxHistory) {
 		this.maxHistory = maxHistory;
 	}
-	
+
 }
